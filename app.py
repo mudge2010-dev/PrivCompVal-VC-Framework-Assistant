8bd6f6af278b3ff0555616554458372927e7b8f3
mudge2010-dev <274882284+mudge2010-dev@users.noreply.github.com>
2026-09-02 19:28:15 -0400

    app.py

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ import openai
 
 # 1. Page Configuration
 st.set_page_config(
-    page_title="Custom Research Assistant", 
+    page_title="Custom Private Company and Venture Capital Research Assistant", 
     page_icon="🤖", 
     layout="centered"
 )
 
-st.title("Custom Assistant")
+st.title("Custom Private Company and Venture Capital Research Assistant")
 st.caption("Powered by Perplexity API")
-
+#3. Main Page Description 
+st.markdown("""This VC Assistant will help users build fluency in: Venture Capital, Investment analysis,  Fundraising, Startup due diligence. You start by entering: "Start Track 1: Introduction to Venture Capital" and follow along the prompts reading the articles and watching the video(s). When you are ready to move to the next step just type " I'm ready for Part 2". A new section will show up with multiple choice questions and practice questions. After that, type "I'm ready for Part 3."""
 # 2. Hidden System Instructions
 # Add your custom prompt instructions here. External users CANNOT view this text.
 HIDDEN_SYSTEM_INSTRUCTIONS = """
