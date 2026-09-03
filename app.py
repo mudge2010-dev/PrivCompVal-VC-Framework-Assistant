c1519886a9d342ba59b1bea83622433cd352fc21
mudge2010-dev <274882284+mudge2010-dev@users.noreply.github.com>
2026-09-03 00:53:15 -0400

    app.py

--- app.py
+++ app.py
@@ -11,7 +11,10 @@ st.set_page_config(
 st.title("Private Company Valuation and Venture Capital Research Assistant")
 st.caption("Powered by Perplexity API")
 
-# 2. Hidden System Instructions
+#2. Main Page Description
+st.markdown("""This VC Assistant will help users build fluency in: Venture Capital, Investment analysis,  Fundraising, Startup due diligence. You start by entering: Start Track 1: Introduction to Venture Capital and follow along the prompts reading the articles and watching the video(s). When you are ready to move to the next step just type:  I'm ready for Part 2. A new section will show up with multiple choice questions and practice questions. After that, type: I'm ready for Part 3""")
+
+# 3. Hidden System Instructions
 # Add your custom prompt instructions here. External users CANNOT view this text.
 HIDDEN_SYSTEM_INSTRUCTIONS = """
 You are a specialized Venture Capital Expert Assistant.
